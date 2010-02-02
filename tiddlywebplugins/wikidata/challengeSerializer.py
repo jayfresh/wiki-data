50fa6df402019e6e5d3a4984031a44e1880fa2cc
Chris Dent <cdent@host47.msm.che.vodafone>
2010-02-02 20:50:28 +0000

    Add 'tiddler' back into the challengeSerializer data
    It was removed in the various bits of cleanup.

--- tiddlywebplugins/wikidata/challengeSerializer.py
+++ tiddlywebplugins/wikidata/challengeSerializer.py
@@ -7,5 +7,5 @@ class Serialization(SerializationInterface):
 
     def tiddler_as(self, tiddler):
         template = templating.get_template(self.environ, 'challenge.html')
-        return template.render(commonVars=templating.common_vars(
-            self.environ))
+        return template.render(tiddler=tiddler,
+                commonVars=templating.common_vars(self.environ))
