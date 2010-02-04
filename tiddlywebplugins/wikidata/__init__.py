6a1130ad55c444c67310ae1907afd8856779f5f8
Chris Dent <cdent@host46.msm.che.vodafone>
2010-02-04 23:26:22 +0000

    Add a stubbed out registration process.
    Works likes this:
    
    If a user goes to /pages/register.html there are presented with a form
    that lives in templates/register.html that askes them for their name,
    user, company.
    
    That information is emailed to the address named in tiddlywebconfig
    wikidata.register_address (the default is currently me, this should change!).
    
    Once the email is sent the user is told they will be contacted later.
    
    That's it for now.
    
    The next step is to provide a user creation form for the admins.

--- tiddlywebplugins/wikidata/__init__.py
+++ tiddlywebplugins/wikidata/__init__.py
@@ -1,15 +1,18 @@
 import logging
+import socket
 
 import tiddlywebplugins.logout
 
 from tiddlywebplugins.wikidata import templating
 from tiddlywebplugins.wikidata.emailAvox import emailAvox
+from tiddlywebplugins.wikidata.sendEmail import send as send_email
 from tiddlywebplugins.wikidata.recordFields import getFields
 from tiddlywebplugins.wikidata import captcha
 from tiddlywebplugins.wikidata.config import config as local_config
 
 from tiddlyweb.util import merge_config
-from tiddlyweb.web.http import HTTP404
+from tiddlyweb.web.http import HTTP404, HTTP302, HTTP303
+from tiddlyweb.web.util import server_base_url
 from tiddlywebplugins.utils import replace_handler, remove_handler
 
 
@@ -22,6 +25,7 @@ def index(environ, start_response):
         ])
     
     return template.render(commonVars=templating.common_vars(environ))
+
 
 def template_route(environ, start_response):
     template_name = environ['wsgiorg.routing_args'][1]['template_file']
@@ -60,6 +64,7 @@ def test_template_route(environ, start_response):
     
     return template.render(commonVars=templating.common_vars(environ))
 
+
 def get_fields_js(environ, start_response):
     template = templating.get_template(environ, 'fields.js.html')
     fields = getFields(environ)
@@ -70,12 +75,37 @@ def get_fields_js(environ, start_response):
     ])
     return template.render(fields=fields)
 
+
 def env(environ, start_response):
 
     from pprint import pformat
 
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [pformat(environ)]
+
+
+def register(environ, start_response):
+    query = environ['tiddlyweb.query']
+    name = query.get('name', [None])[0]
+    company = query.get('company', [None])[0]
+    email = query.get('email', [None])[0]
+    if not (name or company or email):
+        # The form has not been filled out
+        raise HTTP302(server_base_url(environ) + '/pages/register.html')
+    to_address = environ['tiddlyweb.config'].get(
+            'wikidata.register_address', 'cdent@peermore.com')
+    subject = 'Registration Request'
+    body = """
+name: %s
+company: %s
+email: %s
+""" % (name, company, email)
+    try:
+        send_email(to_address, subject, body)
+    except socket.error:
+        logging.debug('%s:%s:%s', to_address, subject, body)
+    raise HTTP303(server_base_url(environ) + '/pages/registered.html')
+
     
 def verify(environ, start_response):
 
@@ -108,6 +138,7 @@ def verify(environ, start_response):
     
     return []
 
+
 def init(config):
     merge_config(config, local_config)
     tiddlywebplugins.logout.init(config)
@@ -120,6 +151,7 @@ def init(config):
     config['selector'].add('/verify', POST=verify)
     config['selector'].add('/lib/fields.js', GET=get_fields_js)
     config['selector'].add('/env', GET=env)
+    config['selector'].add('/register', POST=register)
     replace_handler(config['selector'], '/', dict(GET=index))
     remove_handler(config['selector'], '/recipes')
     remove_handler(config['selector'], '/recipes/{recipe_name}')
@@ -127,5 +159,3 @@ def init(config):
     remove_handler(config['selector'], '/bags')
     remove_handler(config['selector'], '/bags/{bag_name}')
     remove_handler(config['selector'], '/bags/{bag_name}/tiddlers')
-
-
