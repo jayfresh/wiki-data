58cfb6803ecdf295028e373587c82354e2043481
Jonathan Lister <jonathanlister@Jonathan-Listers-MacBook.local>
2010-02-08 12:11:44 +0000

    fixed maps loader so both maps load in turn

--- static/js/agg.js
+++ static/js/agg.js
@@ -6310,47 +6310,61 @@ $(document).ready(function() {
 			google.load("maps", "2", {
 				"callback" : function() {
 					try {
-					var map;
-					var op_company = window.gMaps.op_company;
-					var company;
-					var addToMap = function(response) {
-						// Retrieve the object
-						var place = response.Placemark[0];
-						// Retrieve the latitude and longitude
-						var point = new google.maps.LatLng(place.Point.coordinates[1],
-						                  place.Point.coordinates[0]);
-						// Center the map on this point
-						map.setCenter(point, 3);
-						map.setZoom(14);
-						// Create a marker
-						var marker = new google.maps.Marker(point);
-						// Add the marker to map
-						map.addOverlay(marker);
-						// Add address information to marker
-						marker.openInfoWindowHtml(company);
-					};
-					// Create new map object
-					map = new google.maps.Map2($(mapSelector));
-					console.log(map);
-					map.addControl(new google.maps.SmallMapControl());
-					map.addControl(new google.maps.MapTypeControl());
-					// Create new geocoding object
-					var geocoder = new google.maps.ClientGeocoder();
-					// Retrieve location information, pass it to addToMap()
-					company = op_company + "<br/>"+ address;
-					geocoder.getLocations(address, addToMap);
+						var map;
+						var op_company = window.gMaps.op_company;
+						var company;
+						var addToMap = function(response) {
+							// Retrieve the object
+							var place = response.Placemark[0];
+							// Retrieve the latitude and longitude
+							var point = new google.maps.LatLng(place.Point.coordinates[1],
+							                  place.Point.coordinates[0]);
+							// Center the map on this point
+							map.setCenter(point, 3);
+							map.setZoom(14);
+							// Create a marker
+							var marker = new google.maps.Marker(point);
+							// Add the marker to map
+							map.addOverlay(marker);
+							// Add address information to marker
+							marker.openInfoWindowHtml(company);
+						};
+						// Create new map object
+						map = new google.maps.Map2($(mapSelector).get(0));
+						map.addControl(new google.maps.SmallMapControl());
+						map.addControl(new google.maps.MapTypeControl());
+						// Create new geocoding object
+						var geocoder = new google.maps.ClientGeocoder();
+						// Retrieve location information, pass it to addToMap()
+						company = op_company + "<br/>"+ address;
+						geocoder.getLocations(address, addToMap);
 					} catch(ex) {
-						console.log(ex);
+						$(mapSelector).html('<p>error loading map</p>');
 					}
 				}
 			});
 		}
+		var toInitialize = {
+			'operational_map' : {
+				address: window.gMaps.op_address,
+				initialized: false
+			},
+			'registered_map' : {
+				address: window.gMaps.reg_address,
+				initialized: false
+			}
+		};
+		var initCount = 2;
 		window.mapsInitialize = function() {
-			if($('#operational_map').is(":visible")) {
-				gLoad('#operational_map',window.gMaps.op_address);
-			} else if($('#registered_map').is(":visible")) {
-				gLoad('#registered_map', window.gMaps.reg_address);
-			} else {
+			$.each(toInitialize, function(id, value) {
+				var selector = '#'+id;
+				if(initCount && $(selector).is(":visible") && !value.initialized) {				
+					value.initialized = true;
+					initCount--;
+					gLoad(selector, value.address);
+				}
+			});
+			if(initCount) {
 				window.setTimeout(window.mapsInitialize,1000);
 			}
 		};
