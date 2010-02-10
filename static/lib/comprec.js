483b15c741858301f5a3dfbafea83b55781fbf88
Jonathan Lister <jonathanlister@Jonathan-Listers-MacBook.local>
2010-02-10 23:17:11 +0000

    converting op/reg state codes and entity types on company detail page

--- static/lib/comprec.js
+++ static/lib/comprec.js
@@ -37,6 +37,53 @@ $(document).ready(function() {
 		});
 		$companyDiv.removeClass('hide').css("visibility", "visible");
 		$('#recordcontainer .tab').eq(0).click();
+		
+		
+		// fix up state codes
+		var findStateMap = function(country) {
+			if(country==='Australia') {
+				return ISO_3166['2:AU'].iso2name;
+			} else if(country==='Canada') {
+				return ISO_3166['2:CA'].iso2name;
+			} else if(country==='USA') {
+				return ISO_3166.usa.iso2name;
+			}
+		};
+		var $op_address_div = $('#op_address_div');
+		var country = $op_address_div.find('.operational_country').text();
+		var stateMap = findStateMap(country);
+		var state;
+		if(stateMap) {
+			state = stateMap[$op_address_div.find('.operational_state').text()];
+			if(state) {
+				$op_address_div.find('.operational_state').text(state);
+			}
+		}
+		var $reg_address_div = $('#reg_address_div');
+		country = $reg_address_div.find('.registered_country').text();
+		stateMap = findStateMap(country);
+		if(stateMap) {
+			state = stateMap[$reg_address_div.find('.registered_state').text()];
+			if(state) {
+				$reg_address_div.find('.registered_state').text(state);
+			}
+		}
+		
+		// fix entity type codes
+		var entity_name_map = {
+			"TP": "Ultimate Parent",
+			"LE": "Subsidiary",
+			"SLE": "Branch",
+			"DIV": "Division"
+		};
+		var entity_code = $('.entity_type').text();
+		if(entity_code) {
+			var entity = entity_name_map[entity_code];
+			if(entity) {
+				$('.entity_type').text(entity);
+			}
+		}
+				
 		var makeAddressText = function(selector) {
 			var $elem = $(selector);
 			return $.trim((//$companyDiv.find('.adr .street-address').text() + " " +
