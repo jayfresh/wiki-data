22c9f8568d7cf1977d6e99e9918cc9b0a12d80ab
Chris Dent <cdent@oxide.local>
2010-03-08 15:59:45 +0000

    Add the start of a deleteavids.sh for lighthouse #150.
    Is a simple script that reads ids on STDIN and calls mysql.

---
+++ script/deleteavids.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Delete the avids from file on STDIN.
+#
+# As yet this is very simple and not hugely
+# robust. It is waiting for test data.
+
+DATABASE=avox
+
+while read AVID; do \
+    echo $AVID
+    echo "DELETE FROM $DATABASE WHERE AVID=$AVID;" | mysql avox
+done
