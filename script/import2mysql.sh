8456713ef8b00ac1ada03769278441eeee770a43
Chris Dent <chris.dent@gmail.com>
2010-06-26 11:54:43 +0100

    Automate handling of data imports a bit more
    This is still not the full suite, but does speed
    things up a bit:
    
    If the name of the imported file contains 'wiki_full', delete
    all the rows from the table, and do a full import. Otherwise
    just do a a delta import.
    
    One the import is done, automatically update the fruitmachine,
    which now looks like this:
    
    <span id="fruitmachine" class="lightborder darkgreen">
        {% set counter = '322618' %}
        {% for i in counter %}
        <img src="/static/test_images/count{{ i }}.png" alt="{{ i }}" />
        {% endfor %}
    </span>
    
    The import script changes the value of counter.

--- script/import2mysql.sh
+++ script/import2mysql.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 # as root
 # copy in the data file to /var/lib/mysql/avox as avox.txt
 # then in /var/lib/mysql/avox run
@@ -7,20 +7,34 @@
 # to make this work we need a way of calculating the value of the
 # extract
 
+FRUITFILE=/home/avox/tiddlywebs/www.wiki-data.com/templates/fruitmachine.html
+
 DATABASE=avox
 DATAUSER=avox
+DATATABLE=avox
 DATAFILE=avox.txt
 DATADIR=/var/lib/mysql/avox
 EXTRACTDIR=${2:-/home/avox/wikidata/avoxjson-s/dataextracts}
 
-# calculate extract
-# XXX not done!!!
+## calculate extract
+## XXX not done!!!
 EXTRACTFILE=${1:?"filename required"}
+
+# If this is a full file, delete all the existing entries.
+echo $EXTRACTFILE | grep 'wiki_full' && echo "delete from $DATATABLE;" \
+    | mysql -u $DATAUSER $DATABASE
 
 cd $DATADIR
 cp $EXTRACTDIR/$EXTRACTFILE $DATAFILE
 
 mysqlimport -u $DATAUSER -r --fields-terminated-by='~' $DATABASE $DATAFILE
-#mysqlimport -r --fields-terminated-by='~' $DATABASE $DATAFILE
 
 rm $DATAFILE
+
+COUNTER=$(echo "select count(*) from $DATATABLE;" | \
+	mysql --skip-column-names -u $DATAUSER $DATABASE)
+
+# update the fruitmachine automagically
+perl -pi -e "s/counter = '\d+'/counter = '$COUNTER'/" $FRUITFILE
+
+echo "Total records: $COUNTER"
