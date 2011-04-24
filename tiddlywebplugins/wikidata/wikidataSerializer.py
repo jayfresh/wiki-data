1605528352a91d10689f491975dc9d947ba0d76d
Chris Dent <chris.dent@gmail.com>
2011-04-24 20:10:23 +0100

    In the serialization for search results unwind the generator.
    This allows tiddler|count in the template to work again.

--- tiddlywebplugins/wikidata/wikidataSerializer.py
+++ tiddlywebplugins/wikidata/wikidataSerializer.py
@@ -20,7 +20,8 @@ class Serialization(HTML_Serializer):
         access_count = self.environ.get('tiddlyweb.mappingsql.access_count', 0) # the number of results I have access to
         index = self.environ.get('tiddlyweb.mappingsql.index', 0)
         template = templating.get_template(self.environ, 'collection.html')
-        return template.render(tiddlers=tiddlers, resultcount=resultcount, access_count=access_count, 
+        # Unwind the generator so we can count it in the template.
+        return template.render(tiddlers=list(tiddlers), resultcount=resultcount, access_count=access_count, 
                 commonVars=templating.common_vars(self.environ), pageDistance=self.environ['tiddlyweb.config']['mappingsql.limit'], queryIndex=index)
 
     def tiddler_as(self, tiddler):
