f826163c5a08977459ab0a3d61e575f8fc3cbf35
Jonathan Lister <jonathan@withjandj.com>
2011-05-19 15:18:54 +0100

    updating version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ AUTHOR = 'Wiki-Data'
 AUTHOR_EMAIL = 'suppoert@wiki-data.com'
 NAME = 'tiddlywebplugins.wikidata'
 DESCRIPTION = 'Packaging of Wiki-Data Project'
-VERSION = '0.84_dev'
+VERSION = '0.85_dev'
 
 
 import os
